fc2b6aa1d8b39c6474bc1b1322b81d9f4496986c
neoayus <ayus43@outlook.com>
2026-03-01 18:21:04 +0530

    feat: save upload file in /uploads

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import shutil 
+import os 
 
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
@@ -9,16 +10,18 @@ app = FastAPI()
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
 
+  upload_path = os.path.join("uploads", file.filename)
+
   # save file  
-  with open(file.filename, "wb") as buffer: 
+  with open(upload_path, "wb") as buffer: 
     shutil.copyfileobj(file.file, buffer)
 
   # convert file 
-  new_file = convert_srt(file.filename)
+  new_file = convert_srt(upload_path)
   
   # return converted file 
   return FileResponse(
     path=new_file,
-    filename=new_file, 
+    filename=os.path.basename(new_file),
     media_type="text/plain"
   )
